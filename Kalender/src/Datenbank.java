d5f8152a510ad221296122d3d4bf1a5e26fe7396
saidctn <muhammed_said.cetin@smail.th-koeln.de>
2023-11-14 13:27:40 +0100

    Datenbank

--- Kalender/src/Datenbank.java
+++ Kalender/src/Datenbank.java
@@ -1,15 +1,100 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
 public class Datenbank {
-    public static void displayData() {
+    // Verbindungsattribute
+    private final String url = "jdbc:postgresql://localhost/GuiJava";
+    private final String user = "postgres";
+    private String password;
+    public Datenbank(String password){
+        this.password = password;
+    }
+    public void createTable(){
 
     }
-    public static void deleteData(String name) {
-
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
     }
-    public static void insertData(String name, int age) {
 
+    /**
+     * Display actor
+     *
+     * @param rs
+     * @throws SQLException
+     */
+    private void displayActor(ResultSet rs) throws SQLException {
+        while (rs.next()) {
+            System.out.println(rs.getString("actor_id") + "\t"
+                    + rs.getString("first_name") + "\t"
+                    + rs.getString("last_name"));
+
+        }
     }
-    public static void createTable() {
+    /**
+     * Get all rows in the actor table
+     */
+    public void getActors() {
+
+        String SQL = "SELECT actor_id,first_name, last_name FROM actor";
+
+        try (Connection conn = connect();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(SQL)) {
+            // display actor information
+            displayActor(rs);
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    /**
+     * Find actor by his/her ID
+     *
+     * @param actorID
+     */
+    public void findActorByID(int actorID) {
+        String SQL = "SELECT actor_id,first_name,last_name "
+                + "FROM actor "
+                + "WHERE actor_id = ?";
+
+        try (Connection conn = connect();
+             PreparedStatement pstmt = conn.prepareStatement(SQL)) {
+
+            pstmt.setInt(1, actorID);
+            ResultSet rs = pstmt.executeQuery();
+            displayActor(rs);
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void closeConnection(Connection connection) {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+                System.out.println("Connection closed.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error while closing connection: " + e.getMessage());
+        }
+    }
+    public static void main(String[] args) {
+        Datenbank app = new Datenbank("thkoln");
+        app.connect();
         
-    }
+        app.getActors();
 
+        }
 }
+
--- Kalender/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        MitgliedHinzufuegen m1=new MitgliedHinzufuegen();
-        m1.gui();
-    }
-}
