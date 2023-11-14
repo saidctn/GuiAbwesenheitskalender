b45d7846e37d97f9640047478dfa81c544618179
saidctn <muhammed_said.cetin@smail.th-koeln.de>
2023-11-14 08:48:44 +0100

    Datenbank schnittstellen

---
+++ Kalender/src/Datenbank.java
@@ -0,0 +1,15 @@
+public class Datenbank {
+    public static void displayData() {
+
+    }
+    public static void deleteData(String name) {
+
+    }
+    public static void insertData(String name, int age) {
+
+    }
+    public static void createTable() {
+        
+    }
+
+}
