0017ed566ac62ef93a0a58bccadc1a7be5264c68
cokoh <christopher__warren.okoh@smail.th-koeln.de>
2023-11-20 09:29:26 +0100

    keine ahnung

--- Kalender/src/BorderLayoutMitSplitPane.java
+++ Kalender/src/BorderLayoutMitSplitPane.java
@@ -15,8 +15,9 @@ public class BorderLayoutMitSplitPane extends JFrame {
 
 
         // Erstelle die Listen spätere implementierung mit buttons
-        String[] mitgliederDaten = {"affe"};
+       String[] mitgliederDaten = {"affe"};
         String[] kompetenzDaten = {"Kompetenz 1", "Kompetenz 2", "Kompetenz 3", "Kompetenz 4", "Kompetenz 5"};
+
 
         JList<String> mitgliederListe = new JList<>(mitgliederDaten);
         JList<String> kompetenzListe = new JList<>(kompetenzDaten);
